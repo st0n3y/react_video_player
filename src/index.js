5c4867faca269fb7c9ed512eaeeacbcb09b8f868
David MacKintosh <dgawmackintosh@gmail.com>
2018-07-26 16:46:04 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyDXyBn5NX7YWEOsLDoZdGbBDpy18mxA4XY';
+
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
